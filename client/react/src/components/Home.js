db22f89d4def9940bf31501ad76c886fcf44ea8e
tzvika-naor <zvinaor@gmail.com>
2021-02-02 03:45:34 +0200

    fixing the HOME component

--- client/react/src/components/Home.js
+++ client/react/src/components/Home.js
@@ -1,8 +1,14 @@
 function Home (props) {
     return (
-        <div style={{ backgroundColor: "rgb(100,100,100)", marginTop: "0" }}>
-            {/* <h1>Welcome To Out store</h1> */}
-            {/* <img src="/images/cellular.png " style={{width:"50%" , height:"800px" , marginLeft:"25%", marginTop:"40px"  , marginBottom:"50px"}} alt="" /> */}
+        <div style={{height:"100vh"}}>
+            <h1  className="d-flex justify-content-center" >SMARTPHONES ONLINE STORE</h1>
+            <h3  className="d-flex justify-content-center" >best shoping expirence online</h3>
+
+            <div className="d-flex justify-content-start" >  
+                <img className="d-flex align-self-center" src="/images/Mobile7.jpg" alt="" style={{ width:"33.33%", marginTop:"5%"}}/>
+                <img className="d-flex align-self-center" src="/images/Mobile8.jpg" alt="" style={{ width:"33.33%", marginTop:"5%"}}/>
+                <img className="d-flex align-self-center" src="/images/Mobile9.jpg" alt="" style={{ width:"33.33%", marginTop:"5%"}}/>
+            </div>
         </div>
     )
 }
