a0ce11b7fc16c49340ac3b546da9e479fae4850a
tzvika-naor <zvinaor@gmail.com>
2021-02-21 22:54:30 +0200

    saving

--- client/dashboard/src/app/header/users-counter/users-counter.component.ts
+++ client/dashboard/src/app/header/users-counter/users-counter.component.ts
@@ -7,10 +7,10 @@ import { RealtimeService } from 'src/app/realtime.service';
   styleUrls: ['./users-counter.component.css']
 })
 export class UsersCounterComponent implements OnInit {
-  usersCounter : Number = 0;
-  
-  constructor(private realtimeService: RealtimeService) { 
-    this.realtimeService.currentUsersCounter.subscribe(counter => this.usersCounter = counter)
+  usersCounter = 0;
+
+  constructor(private realtimeService: RealtimeService) {
+    this.realtimeService.currentUsersCounter.subscribe(counter => this.usersCounter = counter);
   }
 
   ngOnInit(): void {
