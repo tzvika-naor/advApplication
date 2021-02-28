d4161b254c79c3ffac4a619e4eb52a78f0fc7afd
tzvika-naor <zvinaor@gmail.com>
2021-02-28 17:51:15 +0200

    saving

--- server/app.js
+++ server/app.js
@@ -82,7 +82,7 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static('../client/dashboard/dist/angular'));
 
     app.get('*', (req,res) => {
-        res.sendFile(path.resolve((__dirname,'/../client/react','build','index.html')))
+        res.sendFile(path.resolve(('../client/react','build','index.html')))
     })
 }
 
